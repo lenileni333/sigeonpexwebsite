ed2a5910054a55992a3d619ac855b522aa1ba9c3
lenileni333 <danielbarkho06@gmail.com>
2026-06-28 22:39:15 +1200

    Add files via upload

---
+++ Sigeon Pex/otherButtons.js
@@ -0,0 +1,9 @@
+/* BUTTONS - PEXIA-HISTORY.HTML */
+
+const viewManuscriptBtn = document.getElementById("viewManuscriptButton");
+
+/* CODE */
+
+viewManuscriptBtn.addEventListener("click", function() {
+    window.location.href = "verityManuscript.html"
+});
