487a22e16eec0673b0a5f7e804e05457603a7b7a
lenileni333 <danielbarkho06@gmail.com>
2026-06-29 10:46:51 +1200

    Delete Sigeon Pex directory

--- Sigeon Pex/otherButtons.js
+++
@@ -1,9 +0,0 @@
-/* BUTTONS - PEXIA-HISTORY.HTML */
-
-const viewManuscriptBtn = document.getElementById("viewManuscriptButton");
-
-/* CODE */
-
-viewManuscriptBtn.addEventListener("click", function() {
-    window.location.href = "verityManuscript.html"
-});
